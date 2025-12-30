8f0871c4018e6f6e60718059b32da66554b13294
ahmadalwan <madajadeh2023@gmail.com>
2025-12-30 10:42:23 +0700

    Membuat data didalam file index.js

---
+++ pertemuan4/index.js
@@ -0,0 +1,8 @@
+import fs from "fs";
+
+/**
+ * Susunan sintax menulis file secara Synchronous
+ * fs.writeFileSync("nama_file", "isi file/datanya")
+ */
+
+fs.writeFileSync("tes.txt", "Belajar menulis file menggunakkan node JS")
