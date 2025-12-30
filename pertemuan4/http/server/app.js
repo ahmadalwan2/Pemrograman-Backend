35e9a95c3e305180e84a808a20b59674fababec8
ahmadalwan <madajadeh2023@gmail.com>
2025-12-30 13:58:47 +0700

    Merapihkan data difile app.js

--- pertemuan4/http/server/app.js
+++ pertemuan4/http/server/app.js
@@ -14,14 +14,29 @@ const server = http.createServer((req, res) => {
         // res.end("Ini data yang dikirimkan dari server, buat client");
 
         // --- Ini simulasi menggunakkan kondisi if-else ---
-        if (req.url ==="/data") {
-            res.setHeader("Content-Type", "text/plain");
-            res.write("Ini data yang dikirmkan dari server dan client");
-            res.end();
-        }   else {
-            res.write("Selamat datang");
-            res.end();
-        }
+        // if (req.url ==="/data") {
+        //     res.setHeader("Content-Type", "text/plain");
+        //     res.write("Ini data yang dikirmkan dari server dan client");
+        //     res.end();
+        // }   else {
+        //     res.write("Selamat datang");
+        //     res.end();
+        // }
+
+        // --- Ini simulasi menggunakkan type filenya JSON ---
+            res.setHeader("Content-Type", "application/json");
+            res.setHeader("Content-Type", "application/json"); 
+        if (req.url === '/data') { 
+            res.write(JSON.stringify({ message: "sukses" })); 
+            res.end(); } 
+        else if (req.url === "/user") { 
+        const user = { id: 1, nama: "Ahmad", hobi: ["ngoding", "berkultum", "membaca"] }; 
+            res.write(JSON.stringify(user)); 
+            res.end(); } 
+        else { 
+            res.writeHead(404); 
+            res.end(JSON.stringify({ 
+            error: "Endpoint tidak ditemukan" })); }
 });
 
 server.listen(3000, () => {
