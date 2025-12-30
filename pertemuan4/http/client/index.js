26e14b27eccca338d7e2f39cc2d7ed9aa71f738b
ahmadalwan <madajadeh2023@gmail.com>
2025-12-30 14:42:12 +0700

    Membuat 2 versi didalam file index.js

--- pertemuan4/http/client/index.js
+++ pertemuan4/http/client/index.js
@@ -9,27 +9,41 @@ const request = http.request (
     "https://jsonplaceholder.typicode.com/users",
     (res) => {
     
-    res.on("data", (data) => {
-        console.log(data.toString());
-    });
+    /**
+     * Ini yang versi 1 nya
+     * Komentar salahsatu versinya ya
+     */
 
-    // - JIka ada error ketika menampilkan data API
-    res.on("error", (err) => {
-        console.log(err.message);
-    });
+    // res.on("data", (data) => {
+    //     console.log(data.toString());
+    // });
+
+    // // - JIka ada error ketika menampilkan data API
+    // res.on("error", (err) => {
+    //     console.log(err.message);
+    // });
 
     /**
      * Ini versi 2 dalam membaca data 
      * Data yang didapatkan melalui event "data" itu sebenarnya masih belum bentuk laporan / chunk,
      * Oleh karena itu perlu di gabungkan secara manual agar datanya  menjadi data yang utuh
      */
+
+    let data = "";
+    res.on("data", (chunk) => {
+        data += chunk
+    });
+    res.on("end", () => {
+        console.log(JSON.parse(data));
+    })
     });
 
 /**
  * Ini digunakkan untuk kalau ternyata pas ngakses ke server itu ada error
  * Contoh : waktu tertalu lama (timeout), url nya salah
  */
-request.on("error", (err) => {
+request.on("error", (
+    err) => {
     console.log("Error", err.message);
 })
 // - Digunakkan untuk mengakhiri request
