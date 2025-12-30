853acc381b60f3f74060775897d56991f21017b0
ahmadalwan <madajadeh2023@gmail.com>
2025-12-30 13:40:28 +0700

    Menambahkan logika sederhana didalam file app.js

--- pertemuan4/http/server/app.js
+++ pertemuan4/http/server/app.js
@@ -10,9 +10,18 @@ const server = http.createServer((req, res) => {
 // - Ini untuk cek statusCode
         console.log(req.statusCode);
 
-        res.setHeader("Content-Type", "text/plain");
-        res.end("Ini data yang dikirimkan dari server, buat client");
-})
+        // res.setHeader("Content-Type", "text/plain");
+        // res.end("Ini data yang dikirimkan dari server, buat client");
+
+        if (req.url ==="/data") {
+            res.setHeader("Content-Type", "text/plain");
+            res.write("Ini data yang dikirmkan dari server dan client");
+            res.end();
+        }   else {
+            res.write("Selamat datang");
+            res.end();
+        }
+});
 
 server.listen(3000, () => {
         console.log("Server berjalan.........");
